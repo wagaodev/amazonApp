5d68e746831d688f33194e20fba50b713639a33a
wcfx <wcfx.dev@gmail.com>
2021-07-19 22:39:49 -0300

    chore: add eslint babel prettier e configs

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, useColorScheme } from 'react-native';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const App: React.FC = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    </SafeAreaView>
+  );
+};
+
+export default App;
